229e9c1f04430a6d7ce29c0478e146bbf92bcfe9
Ferdinand <ferdinand.froidefont@gmail.com>
2026-03-29 22:04:38 +0200

    Remove redundant closing div in ItineraryFeed component for cleaner markup

--- src/pages/ItineraryFeed.tsx
+++ src/pages/ItineraryFeed.tsx
@@ -101,7 +101,6 @@ export default function ItineraryFeed() {
                 ))}
               </div>
             )}
-            </div>
           </div>
         </ScrollArea>
       </IosFixedPageHeaderShell>
